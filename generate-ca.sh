c10d4244e7abfdeb8eb71c1a198de7671c4409ea
Dave Keen <dev@ruffness.com>
2020-08-28 13:59:48 +0200

    sort of works

--- generate-ca.sh
+++ generate-ca.sh
@@ -19,8 +19,9 @@ mkdir -p $TARGET_DIR
 
 # Create a key and a CA for our organisation and put them in a folder
 mkdir $TARGET_DIR
-openssl genrsa -passout pass:$PASSWORD -des3 -out ${TARGET_DIR}/${ORGANISATION}CA.key 2048
-openssl req -passin pass:$PASSWORD -x509 -new -nodes -key ${TARGET_DIR}/${ORGANISATION}CA.key -sha256 -days 36500 -subj "/CN=${ORGANISATION} DEV CA/O=${ORGANISATION}/C=CH" -out ${TARGET_DIR}/${ORGANISATION}CA.pem
+# openssl genrsa -passout pass:$PASSWORD -des3 -out ${TARGET_DIR}/ca.key 2048
+# openssl req -passin pass:$PASSWORD -x509 -new -nodes -key ${TARGET_DIR}/ca.key -sha256 -days 825 -subj "/CN=${ORGANISATION} DEV CA/O=${ORGANISATION}/C=CH" -out ${TARGET_DIR}/ca.pem
+openssl req -config openssl.cnf -x509 -new -days 825 -subj "/CN=${ORGANISATION} DEV CA/O=${ORGANISATION}/C=CH" -out ca.crt
 
 # Create a bash script for generating certificates
 SCRIPT=${TARGET_DIR}/generate_certificate.sh
@@ -39,7 +40,7 @@ echo "SERVERS=\${SERVERS:-DNS:localhost,IP:192.168.43.158,IP:192.168.8.19}" >> $
 echo "" >> $SCRIPT
 echo "openssl genrsa -passout pass:$PASSWORD -out \$CERTIFICATE_NAME.key 2048" >> $SCRIPT
 echo "openssl req -passin pass:$PASSWORD -new -key \$CERTIFICATE_NAME.key -subj \"/C=CH\" -out \$CERTIFICATE_NAME.csr" >> $SCRIPT
-echo "openssl x509 -req -passin pass:$PASSWORD -in \$CERTIFICATE_NAME.csr -CA ${ORGANISATION}CA.pem -CAkey ${ORGANISATION}CA.key -CAcreateserial -out \$CERTIFICATE_NAME.crt -days 825 -sha256 -extfile <(printf \"subjectAltName=\$SERVERS\")" >> $SCRIPT
+echo "openssl x509 -req -passin pass:$PASSWORD -in \$CERTIFICATE_NAME.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out \$CERTIFICATE_NAME.crt -days 825 -sha256 -extfile <(printf \"subjectAltName=\$SERVERS\")" >> $SCRIPT
 
 chmod +x ${TARGET_DIR}/generate_certificate.sh
 
